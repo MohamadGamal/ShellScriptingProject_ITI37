e172c41af7e54ad32adb31e7e38f1238de137c44
Mgamal <Mohamad.gamal.abdelhay@gmail.com>
2017-01-14 11:37:22 +0200

    Added parser

--- parser.sh
+++ parser.sh
@@ -7,23 +7,90 @@
 #meta processor
 #
 function clearbeg(){
-str=$1
+typeset str=$1
 typeset i=0;
 while [ "${str:i:${#2}}" = "$2" ]
 do
-((i += 1));
+((i += ${#2}));
 done;
 
 echo ${str:(i)}
 
 }
-echo ${str:i:1}
-str="            create    table {msd:string,mg:number,sss:string}"
+
+function strfrom(){
+typeset str=$1
+typeset i=0;
+while [ "${str:i:${#2}}" != "$2" ]
+do
+((i += ${#2}));
+done;
+
+echo ${str:(i)}
+
+}
+
+function delcha(){
+typeset str=$1
+typeset strtemp="";
+typeset -i i=0;
+while [ $i -lt ${#str}  ]
+do
+if [ "${str:i:${#2}}" != "$2" ]
+then
+strtemp=$strtemp${str:i:${#2}};
+fi
+((i += ${#2}));
+done;
+echo $strtemp
+
+}
+function delcha(){
+typeset str=$1
+typeset strtemp="";
+typeset -i i=0;
+while [ $i -lt ${#str}  ]
+do
+if [ "${str:i:${#2}}" != "$2" ]
+then
+strtemp=$strtemp${str:i:${#2}};
+fi
+((i += ${#2}));
+done;
+echo $strtemp
+
+}
+#echo ${str:i:1}
+str="            create       table     {         msd:  string,mg:  number,  sss:string ,a:pk}"
 
 #str=`clearbeg "$str" " "  `;
-ord1=`echo $str | cut -f3 -d" " `
+echo $str
 
-echo $ord1
+
+ord1=`echo $str | cut -f3 -d" " `
+order=`strfrom "$str" "{"`
+supord=`delcha "$order" " "`
+supord=`delcha "$supord" "{"`
+meta=`delcha "$supord" "}"`
+echo "SUP"$meta;
+c=(`echo $meta | awk -F, '{
+    
+    i=1;
+    while(i<=NF){
+    split($i,a,":");
+    print a[2];
+    i++;
+    }
+    
+    }
+    END{
+        print $(i-1);
+    }'`)
+for typer in ${c[*]};
+do
+echo $typer;
+echo "Here's what you gonna do '"
+done
 #echo $str;
 
 
